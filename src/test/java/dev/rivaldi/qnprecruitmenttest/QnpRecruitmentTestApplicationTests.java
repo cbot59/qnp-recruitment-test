e940971efc552b02ae52650f13010e483de06f29
Rivaldi <cbot59@gmail.com>
2024-02-29 23:33:20 +0700

    feat: setup spring boot project

---
+++ src/test/java/dev/rivaldi/qnprecruitmenttest/QnpRecruitmentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rivaldi.qnprecruitmenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnpRecruitmentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
