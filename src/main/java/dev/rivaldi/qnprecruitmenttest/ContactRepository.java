33062bc1f547f776cb3368f438599933fc419540
Rivaldi <cbot59@gmail.com>
2024-02-29 23:33:41 +0700

    feat: task no 1

---
+++ src/main/java/dev/rivaldi/qnprecruitmenttest/ContactRepository.java
@@ -0,0 +1,9 @@
+package dev.rivaldi.qnprecruitmenttest;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "contacts", path = "contacts")
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Long>, CrudRepository<Contact, Long> {
+}
